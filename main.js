99fe6910e083daf69d312381822326ae307413be
Dakshina Scott <dakshina.scott@gmail.com>
2017-02-26 19:48:13 +0000

    Adds highlighting for current menu item

--- main.js
+++ main.js
@@ -1,8 +1,24 @@
 $(document).ready(function() {
   $('.info').each(function(element) {
     $(this).click(function() {
-      $(this).parent().prev().prev().toggle("slow");
-      $(this).toggleClass("info-color");
+      $(this).parent().prev().prev().toggle('slow');
+      $(this).toggleClass('info-color');
     });
+  });
+
+    var sections = $('section');
+    var sectionIds = sections.map(function() {
+      return this.id;
+    });
+  var menuItems = $('.nav-main a');
+  var menuHeight = $('.nav-main').outerHeight();
+  $(document).scroll(function() {
+    var cutoff = $(window).scrollTop() + menuHeight - 5;
+    var currentSection = sections.filter(function() {
+      return $(this).offset().top < cutoff + $(window).height() / 4;
+    }).last();
+    var menuItem = $('a[href="#' + currentSection.attr('id') + '"]');
+    menuItems.removeClass('current');
+    menuItem.addClass('current');
   });
 })
