f2525f89327e4324fa918e84c952e894e8274635
Dakshina Scott <dakshina.scott@gmail.com>
2018-01-24 11:42:29 +0000

    splits into separate pages

--- main.js
+++ main.js
@@ -5,45 +5,4 @@ $(document).ready(function() {
       $(this).toggleClass('info-color');
     });
   });
-
-    var sections = $('section');
-    var sectionIds = sections.map(function() {
-      return this.id;
-    });
-  var menuItems = $('.nav-main a');
-  var menuHeight = $('.nav-main').outerHeight();
-  $(document).scroll(function() {
-    var cutoff = $(window).scrollTop() + menuHeight - 5;
-    var nearEnd = 
-      $(window).scrollTop() + $(window).height() > $(document).height() - 100;
-    var nearTop = $(document).scrollTop() < 30;
-    var currentSection = sections.filter(function() {
-      return $(this).offset().top < cutoff + $(window).height() / 4;
-    }).last();
-    var menuItem = $('a[href="#' + currentSection.attr('id') + '"]');
-    menuItems.removeClass('current');
-    if (nearEnd) {
-      $('.nav-main .btn').last().addClass('current');
-    }
-    else {
-      menuItem.addClass('current');
-    }
-    if (!nearTop) {
-      $('header').addClass('nav-reduce');
-    }
-    else {
-      $('header').removeClass('nav-reduce');
-    }
-
-  });
-
-  $('.nav-main a').click(function(el) {
-    var id = $(this).attr('href').slice(1);
-    var offset = $('#' + id).offset().top;
-    $('html, body').animate({
-      scrollTop: offset
-    }, 200);
-    return false;
-  });
-
 })
